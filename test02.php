787a7f18ab3df6741eae9f532aab1366eac93222
tunsatanic13 <60635777+tunsatanic13@users.noreply.github.com>
2020-07-17 11:09:41 +0700

    Add files via upload

---
+++ test02.php
@@ -0,0 +1,27 @@
+<?php
+for($i=1;$i<=5;$i++){
+    for ($j=5; $j>=$i; $j--) {
+        print("&nbsp;&nbsp;");
+    }
+    for ($x=1; $x<=$i ; $x++) {
+        print("o");
+    }
+    print("<br/>");
+}
+print("<br/>")
+?>
+
+<?php
+for($i=1;$i<=5;$i++){
+    for ($j=5; $j>=$i; $j--) {
+        print("&nbsp;&nbsp;");
+    }
+    for ($x=1; $x<$i ; $x++) {
+        print("o");
+    }
+    for ($z=1; $z <=$i ; $z++) {
+        print("o");
+    }
+    print('<br/>');
+}
+?>
